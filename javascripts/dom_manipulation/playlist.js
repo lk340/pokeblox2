6635c95848bb07f58650656e34c0932b847e985d
Loyd Kim <loyd.hkim@gmail.com>
2019-02-24 18:04:58 -0500

    Add looping feature to playlist

--- javascripts/dom_manipulation/playlist.js
+++ javascripts/dom_manipulation/playlist.js
@@ -5,77 +5,90 @@ export const playlist = () => {
     currentSong.pause();
     currentSong = document.getElementById("tears-of-life");
     currentSong.play();
+    currentSong.loop = true;
   });
 
   document.getElementById("johtoOpening").addEventListener("click", () => {
     currentSong.pause();
     currentSong = document.getElementById("johto-opening");
     currentSong.play();
+    currentSong.loop = true;
   });
   
   document.getElementById("viridianCity").addEventListener("click", () => {
     currentSong.pause();
     currentSong = document.getElementById("viridian-city");
     currentSong.play();
+    currentSong.loop = true;
   });
   
   document.getElementById("viridianForest").addEventListener("click", () => {
     currentSong.pause();
     currentSong = document.getElementById("viridian-forest");
     currentSong.play();
+    currentSong.loop = true;
   });
   
   document.getElementById("battleTeamRocket").addEventListener("click", () => {
     currentSong.pause();
     currentSong = document.getElementById("battle-team-rocket");
     currentSong.play();
+    currentSong.loop = true;
   });
   
   document.getElementById("route47").addEventListener("click", () => {
     currentSong.pause();
     currentSong = document.getElementById("route-47");
     currentSong.play();
+    currentSong.loop = true;
   });
   
   document.getElementById("gscChampion").addEventListener("click", () => {
     currentSong.pause();
     currentSong = document.getElementById("gsc-champion");
     currentSong.play();
+    currentSong.loop = true;
   });
   
   document.getElementById("dppChampion").addEventListener("click", () => {
     currentSong.pause();
     currentSong = document.getElementById("dpp-champion");
     currentSong.play();
+    currentSong.loop = true;
   });
   
   document.getElementById("nTheme").addEventListener("click", () => {
     currentSong.pause();
     currentSong = document.getElementById("n-theme");
     currentSong.play();
+    currentSong.loop = true;
   });
   
   document.getElementById("snowbelleCity").addEventListener("click", () => {
     currentSong.pause();
     currentSong = document.getElementById("snowbelle-city");
     currentSong.play();
+    currentSong.loop = true;
   });
   
   document.getElementById("usumTheme").addEventListener("click", () => {
     currentSong.pause();
     currentSong = document.getElementById("usum-theme");
     currentSong.play();
+    currentSong.loop = true;
   });
   
   document.getElementById("originalTetris").addEventListener("click", () => {
     currentSong.pause();
     currentSong = document.getElementById("original-tetris");
     currentSong.play();
+    currentSong.loop = true;
   });
   
   document.getElementById("tetris_99").addEventListener("click", () => {
     currentSong.pause();
     currentSong = document.getElementById("tetris99");
     currentSong.play();
+    currentSong.loop = true;
   });
 };

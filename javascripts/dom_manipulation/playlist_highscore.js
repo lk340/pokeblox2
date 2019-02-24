d99e14e319872124d86ae8e496b97def81e0d5e4
Loyd Kim <loyd.hkim@gmail.com>
2019-02-24 17:05:10 -0500

    Finish CSS for now

--- javascripts/dom_manipulation/playlist_highscore.js
+++ javascripts/dom_manipulation/playlist_highscore.js
@@ -10,32 +10,34 @@ export const playlistHighscore = () => {
     const playlistSongsClass = playlistSongs[0];
     const playlistArrowClass = playlistArrow[0];
 
-    if (highScoreClass === "high-score" && playlistClass === "playlist" && playlistSongsClass === "playlist-songs" && playlistArrowClass === "playlist-enlarge") {
-      highScore.remove("high-score");
-      highScore.add("high-score-enlarged");
+    if (window.innerWidth > 1679) {
+      if (highScoreClass === "high-score" && playlistClass === "playlist" && playlistSongsClass === "playlist-songs" && playlistArrowClass === "playlist-enlarge") {
+        highScore.remove("high-score");
+        highScore.add("high-score-enlarged");
+    
+        playlist.remove("playlist");
+        playlist.add("playlist-enlarged");
+    
+        playlistSongs.remove("playlist-songs");
+        playlistSongs.add("playlist-songs-enlarged");
+    
+        playlistArrow.remove("playlist-enlarge");
+        playlistArrow.add("playlist-small");
+      }
   
-      playlist.remove("playlist");
-      playlist.add("playlist-enlarged");
-  
-      playlistSongs.remove("playlist-songs");
-      playlistSongs.add("playlist-songs-enlarged");
-  
-      playlistArrow.remove("playlist-enlarge");
-      playlistArrow.add("playlist-small");
-    }
-
-    else if (highScoreClass === "high-score-enlarged" && playlistClass === "playlist-enlarged" && playlistSongsClass === "playlist-songs-enlarged" && playlistArrowClass === "playlist-small") {
-      highScore.remove("high-score-enlarged");
-      highScore.add("high-score");
-  
-      playlist.remove("playlist-enlarged");
-      playlist.add("playlist");
-  
-      playlistSongs.remove("playlist-songs-enlarged");
-      playlistSongs.add("playlist-songs");
-  
-      playlistArrow.remove("playlist-small");
-      playlistArrow.add("playlist-enlarge");
+      else if (highScoreClass === "high-score-enlarged" && playlistClass === "playlist-enlarged" && playlistSongsClass === "playlist-songs-enlarged" && playlistArrowClass === "playlist-small") {
+        highScore.remove("high-score-enlarged");
+        highScore.add("high-score");
+    
+        playlist.remove("playlist-enlarged");
+        playlist.add("playlist");
+    
+        playlistSongs.remove("playlist-songs-enlarged");
+        playlistSongs.add("playlist-songs");
+    
+        playlistArrow.remove("playlist-small");
+        playlistArrow.add("playlist-enlarge");
+      }
     }
   });
 };

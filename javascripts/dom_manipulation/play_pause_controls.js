49a205a64c2d8a9c4f7387537d52079aacfb9037
Loyd Kim <loyd.hkim@gmail.com>
2019-03-11 14:47:27 -0400

    Fix issue with guide modal not closing when starting game with 'q' hotkey

--- javascripts/dom_manipulation/play_pause_controls.js
+++ javascripts/dom_manipulation/play_pause_controls.js
@@ -2,6 +2,12 @@ export const playPause = (currentPiece, shadowPiece, game) => {
   document.addEventListener("keydown", event => {
     switch(event.which) {
       case 81: // q
+        const guideModal = document.getElementById("guide-modal").classList;
+        if (guideModal[0] === "guide-modal") {
+          guideModal.remove("guide-modal");
+          guideModal.add("guide-modal-close");
+        }
+      
         const initialGameScreen = document.getElementById("initial-game-screen").classList;
         if (initialGameScreen[0] === "initial-game-screen") {
           initialGameScreen.remove("initial-game-screen");
